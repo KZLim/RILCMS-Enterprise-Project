0fae7b39a3ea6c02a605b83ffdaed505a75c74de
KZLim <p20012425@student.newinti.edu.my>
2023-05-25 21:37:29 +0800

    Modification to homepage
    homepage completed along with css changes

--- homepage.php
+++ homepage.php
@@ -2,7 +2,7 @@
 
 <html>
     <head>
-        <title>Hello World</title>
+        <title>Thunder Robotics | Homepage</title>
         <meta charset = "utf-8">
         <link rel="stylesheet" href="main_theme.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
@@ -40,27 +40,68 @@
         </div>
 
         <div class="flex-container">
-            <div class="flex-item-left">1</div>
-            <div class="flex-item-right">2</div>
+            <div class="flex-item-left">
+                <img src="images/robotics kit.png" height="500px" width="500px">
+            </div>
+
+            <div class="flex-item-right" style="background-color: #3B8BEB; color: white;">
+                <h2 style="margin-top: 100px;">Hands - On Learning</h2>
+                <p style="font-size: 25px; padding: 20px;">
+                    We are a strong believer of learning by doing. Hands on experince is crucial for learning new technology.
+                    By doing, it is can spark more interest and creativity. At Thunder Robotics, we have kits for all classes 
+                    you register and attend. Letting you on the first hand experience on all our projects.
+                </p>
+
+            </div>
         </div>
 
         <div class="flex-container">
-            <div class="flex-item-left">1</div>
-            <div class="flex-item-right">2</div>
+            <div class="flex-item-left" style="background-color: #3B8BEB; color: white">
+                <h2 style="margin-top: 100px;">Interactive Class and Committed Staff</h2>
+                <p style="font-size: 25px; padding: 20px;">
+                    We as technology and robotics lover too are committed to bringing the very best class experience to you
+                    and you kid. Every staff of our center are profesionnaly trained and has experience in the field for
+                    more than 3 years.
+                </p>
+            </div>
+
+            <div class="flex-item-right">
+                <img src="images/interactive class.webp" height="400px" width="500xp">
+            </div>
         </div>
 
         <div class="flex-container">
-            <div class="flex-item-left">1</div>
-            <div class="flex-item-right">2</div>
+            <div class="flex-item-left">
+                <img src="images/comfortable environment.jpg" height="400px" width="500xp">
+            </div>
+            <div class="flex-item-right" style="background-color: #3B8BEB; color: white">
+                <h2 style="margin-top: 100px;">Comfortable classrom across all centers</h2>
+                <p style="font-size: 25px; padding: 20px;">
+                    Comfortable Classroom across all of our centers. Our centers are equip with the upmost comfortable environment
+                    for the best learning experience. Every inch of our center are also daily sanatize to ensure safety</p>
+                </p>
+            </div>
         </div>
 
         <div class="flex-container">
-            <div class="flex-item-left">1</div>
-            <div class="flex-item-right">2</div>
+            <div class="flex-item-left" style="background-color: #3B8BEB; color: white">
+                <h2 style="margin-top: 100px;">A variation of Courses</h2>
+                <p style="font-size: 25px; padding: 20px;">
+                    We offer multiple course in our center acorss Penang. We have beginner class for sparking interest to advance
+                    classes where we get ready for the real deal. </p>
+                </p>
+            </div>
+
+            <div class="flex-item-right">
+                <img src="images/robotic subject.jpg" height="400px" width="500xp">
+            </div>
         </div>
 
         <div class="thirdSection">
-            <h1>test</h1>
+            <h2 style="color: white; padding-top: 40px; text-align: center;">Join Us Today!</h2>
+            <p style="font-size: 25px; padding: 20px; text-align: center; color: white;">
+                    Quickly, grab a spot with us and join us on our robotics journey</p>
+            <a href = "header.php"><button name="gotoRegister" class="glowButton">Register</button></a>
         </div>
     </body>
 </html>
