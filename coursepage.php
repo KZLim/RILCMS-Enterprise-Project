ad5eb5533ffc3dcb3b3ad7c81d77832b49f3d348
lowzhanxian <lowzhanxian9218@gmail.com>
2023-05-26 11:20:52 +0800

    contact us and about us

---
+++ coursepage.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width-device-width" initial-scale="1.0">
+    <link rel="stylesheet" href="./register.css">
+    <link rel="stylesheet" href="coursepage.css">
+    <title>Course Selection | Student</title>
+</head>
+<body>
+    <div class="courseformcontainer">
+        <h1 class="title">Course Selection </h1>
+        
+        <form class="detailform" action="" method="">
+            <select class="input" name="Course Name" >
+                <option value="female">1</option>
+                <option value="male">1</option>
+            </select>
+
+
+            <select class="input" name="Schedule Time" >
+                <option value="female">1</option>
+                <option value="male">1</option>
+            </select>
+
+            <select class="input" name="Branch Name" >
+                <option value="female">1</option>
+                <option value="male">1</option>
+            </select>
+              
+            <br><br>
+                
+                
+            <input name="signin" type="submit" value="Next" class="glowButton">
+        </form>
+    </div>
+    
+    
+    
+
+</body>
+</html>
