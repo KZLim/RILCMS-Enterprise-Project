70c68999e14693d4f1fec57a504252f4c059906a
lowzhanxian <lowzhanxian9218@gmail.com>
2023-05-30 16:19:28 +0800

    coursepage
    coursepage

---
+++ coursepage.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width-device-width" initial-scale="1.0">
+    <link rel="stylesheet" href="./register.css">
+    <link rel="stylesheet" href="coursepage.css">
+    <title>Course Selection | Student</title>
+</head>
+<body>
+    <div class="courseformcontainer">
+        <h1 class="title" style="margin-top:40px;">Course Selection </h1>
+        
+        <form class="detailform" action="" method="" style="padding-top:20px;">
+            <select class="input" name="Course Name" >
+                <option value="Subjects Selection" selected>Subjects Selection</option>
+                <option value="Mechanical Engineering">Mechanical Engineering</option>
+                <option value="Power Electronics">Power Electronics</option>
+                <option value="Electric Drives">Electric Drives</option>
+                
+            </select>
+
+
+            <select class="input" name="Schedule Time" >
+                <option value="Schedule Time" selected>Schedule Time</option>
+                <option value="Monday |9am-11am">Monday |9am-11am</option>
+                <option value="Wednesday |12pm-3pm">Wednesday |12pm-3pm</option>
+                <option value="Thursday |1pm-4pm">Thursday |1pm-4pm</option>
+               
+            </select>
+
+            <select class="input" name="Branch Name" >
+                <option value="Branch Name" selected>Branch Name</option>
+                <option value="Ayer Itam Branch">Ayer Itam Branch</option>
+                <option value="Tanjung Bungah Branch">Tanjung Bungah Branch</option>
+                <option value="Bayan Lepas Branch">Bayan Lepas Branch</option>
+                <option value="Kedah Branch">Kedah Branch</option>
+            </select>
+              
+            <br><br>
+                
+                
+            <input name="signin" type="submit" value="Next" class="glowButton">
+        </form>
+    </div>
+    
+    
+    
+
+</body>
+</html>
