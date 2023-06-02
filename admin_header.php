2226f6be749569a601340ec01d47c848bcd9198d
KZLim <p20012425@student.newinti.edu.my>
2023-06-02 23:44:24 +0800

    Implemented Hardware Asset Viewing
    The entire company hardware asset view has been implemented.

---
+++ admin_header.php
@@ -0,0 +1,20 @@
+<html>
+
+    <head>
+        <title>Admin Header</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="admin_header.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    </head>
+
+    <body>
+        <div class="header">
+            <div class="d-flex justify-content-between">
+                <image src="images/back icon2.png" width="60px" height="70px" class="header-icon">
+                <h1 style="color: white; text-align:center;" class="title">Admin Dashboard</h1>
+                <span></span>
+            </div>
+        </div>
+    </body>
+</html>
