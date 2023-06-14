2052d4dbe67eb94a153fadcad783af0db0284991
lowzhanxian <lowzhanxian9218@gmail.com>
2023-06-14 09:40:02 +0800

    form
    form

---
+++ remove_employee.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width-device-width" initial-scale="1.0">
+    <title>Remove Employee Form</title>
+    <link rel="stylesheet" href="form.css">
+</head>
+<body>
+    
+        <form action="" method="POST" class="remove-employee-container" >
+
+            <h1 class="heading">Remove Employee</h2>
+
+            <div class="container text-center">
+                <div class="row">
+                    <div class="col">
+                        <label for="employeeId" class="employee-form-label" style="margin-left:auto;margin-right:auto;">Employee Id:</label><br>
+                        <input type="text" class="input" id="employeeid" name="employeeID" required><br><br>
+                    </div>
+                </div>
+            </div>
+        
+        <input type="submit" class="glow-Button" value="Remove Employee">
+    </form>
+</body>
+</html>
